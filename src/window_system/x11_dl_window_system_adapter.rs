1a11e68b5e428fddf4ba3cc0bbf2002a8f9fc63b
ultramaxu <ultramaxu@proton.me>
2024-07-05 09:58:45 +0200

    use XGetWMName with XFetchName as fallback instead of XGetWindowProperty

--- src/window_system/x11_dl_window_system_adapter.rs
+++ src/window_system/x11_dl_window_system_adapter.rs
@@ -1,3 +1,4 @@
+use std::ffi::CStr;
 use crate::gateways::{ListWindowsWindowSystemGateway, ScreenShotWindowSystemGateway};
 
 pub struct X11DLWindowSystemAdapter {
@@ -24,7 +25,8 @@ impl X11DLWindowSystemAdapter {
         searched_window_name: &String,
         window: x11_dl::xlib::Window,
     ) -> anyhow::Result<Option<x11_dl::xlib::Window>> {
-        if self.get_window_title(window)? == *searched_window_name {
+        let title = self.get_window_title(window)?;
+        if title.is_some() && title.unwrap() == *searched_window_name {
             return Ok(Some(window));
         }
         Ok(self.iterate_over_window_childrens(
@@ -38,13 +40,16 @@ impl X11DLWindowSystemAdapter {
         window: x11_dl::xlib::Window,
         result: &mut Vec<String>,
     ) -> anyhow::Result<Option<()>> {
-        result.push(self.get_window_title(window)?);
+        let title = self.get_window_title(window)?;
+        if title.is_some() {
+            result.push(title.unwrap());
+        }
 
         self.iterate_over_window_childrens(
             window,
             |child_window| self.list_windows_recursive_helper(child_window, result),
         )?;
-      
+
         Ok(None)
     }
 
@@ -52,7 +57,9 @@ impl X11DLWindowSystemAdapter {
         &self,
         window: x11_dl::xlib::Window,
         mut fun: F,
-    ) -> anyhow::Result<Option<T>> where F: FnMut(x11_dl::xlib::Window) -> anyhow::Result<Option<T>> {
+    ) -> anyhow::Result<Option<T>> where
+        F: FnMut(x11_dl::xlib::Window) -> anyhow::Result<Option<T>>,
+    {
         unsafe {
             let mut root_return: x11_dl::xlib::Window = 0;
             let mut parent_return: x11_dl::xlib::Window = 0;
@@ -87,41 +94,55 @@ impl X11DLWindowSystemAdapter {
     fn get_window_title(
         &self,
         window: x11_dl::xlib::Window,
-    ) -> anyhow::Result<String> {
+    ) -> anyhow::Result<Option<String>> {
+        let mut wm_name: Option<String> = None;
+        wm_name = self.try_XGetWMName(window);
+        // For older versions
+        if wm_name.is_none() {
+            wm_name = self.try_XFetchName(window);
+        }
+        Ok(wm_name)
+    }
+
+    // https://www.x.org/releases/current/doc/libX11/libX11/libX11.html#XGetWMName
+    fn try_XGetWMName(&self, window: x11_dl::xlib::Window) -> Option<String> {
         unsafe {
-            let mut title: Option<String> = None;
-            let mut actual_type_return: x11_dl::xlib::Atom = 0;
-            let mut actual_format_return: std::ffi::c_int = 0;
-            let mut nitems_return: std::ffi::c_ulong = 0;
-            let mut bytes_after_return: std::ffi::c_ulong = 0;
-            let mut data: *mut u8 = std::ptr::null_mut();
+            let mut prop: x11_dl::xlib::XTextProperty = std::mem::zeroed();
 
-            let net_wm_name = std::ffi::CString::new("_NET_WM_NAME".to_string()).unwrap();
-            let utf8_name = std::ffi::CString::new("UTF8_STRING".to_string()).unwrap();
-            let net_wm_atom = (self.xlib.XInternAtom)(self.display, net_wm_name.as_ptr(), x11_dl::xlib::False);
-            let utf8_atom = (self.xlib.XInternAtom)(self.display, utf8_name.as_ptr(), x11_dl::xlib::False);
-            let ret = (self.xlib.XGetWindowProperty)(self.display,
-                                                     window,
-                                                     net_wm_atom,
-                                                     0,
-                                                     8096,
-                                                     x11_dl::xlib::False,
-                                                     utf8_atom,
-                                                     &mut actual_type_return,
-                                                     &mut actual_format_return,
-                                                     &mut nitems_return,
-                                                     &mut bytes_after_return,
-                                                     &mut data as *mut _,
-            );
-            if ret != x11_dl::xlib::Success as i32 {
-                anyhow::bail!("Unable to read the window property of {:#x}", window);
-            }
-            if !data.is_null() {
-                title = Some(std::ffi::CStr::from_ptr(data as *const i8).to_string_lossy().into_owned());
-                (self.xlib.XFree)(data as *mut _);
+            let ret = (self.xlib.XGetWMName)(self.display, window, &mut prop);
+            if ret == 0 {
+                return None;
             }
 
-            Ok(title.unwrap_or(String::from("N/A")))
+            if prop.value.is_null() {
+                return None;
+            }
+
+            let value = Some(CStr::from_ptr(prop.value as *const i8).to_str().unwrap().to_string());
+
+            (self.xlib.XFree)(prop.value as _);
+            value
+        }
+    }
+
+    // According to https://github.com/idunham/xutils/blob/master/xwininfo.c#L487
+    fn try_XFetchName(&self, window: x11_dl::xlib::Window) -> Option<String> {
+        unsafe {
+            let mut data: *mut i8 = std::ptr::null_mut();
+            
+            let ret = (self.xlib.XFetchName)(self.display, window, &mut data);
+            if ret == 0 {
+                return None;
+            }
+
+            if data.is_null() {
+                return None;
+            }
+
+            let value = Some(CStr::from_ptr(data as *const i8).to_str().unwrap().to_string());
+
+            (self.xlib.XFree)(data as _);
+            value
         }
     }
 }
@@ -130,7 +151,7 @@ impl ScreenShotWindowSystemGateway for X11DLWindowSystemAdapter {
     fn find_window(&self, searched_window_name: &String) -> anyhow::Result<Option<u64>> {
         let window = self.find_window_recursive_helper(
             searched_window_name,
-            self.root_win
+            self.root_win,
         )?;
         Ok(window.map(|w| w as _))
     }
