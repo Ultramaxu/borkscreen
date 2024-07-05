c0bbc4915ee1dc53924ee723d87e600d47f5e7fc
ultramaxu <ultramaxu@proton.me>
2024-07-05 09:58:45 +0200

    Initial commit - working proof of concept

---
+++ src/main.rs
@@ -0,0 +1,158 @@
+use anyhow::{bail, Result};
+
+fn take_screenshot(searched_window_name: String) -> Result<()> {
+    unsafe {
+        let xlib = x11_dl::xlib::Xlib::open()?;
+
+        let display = (xlib.XOpenDisplay)(std::ptr::null());
+        if display.is_null() {
+            bail!("Unable to open X server display")
+        }
+        let root_win = (xlib.XDefaultRootWindow)(display);
+
+        let Some(target_window) = find_window_recursive(&xlib, &searched_window_name, root_win, display)? else {
+            bail!("Unable to find the window with title {:?}", searched_window_name)
+        };
+        let image_buffer = get_window_data(&xlib, target_window, display)?;
+
+        image_buffer.save("test.png")?;
+
+        println!("Finished successfully for {:#x}", target_window);
+        return Ok(())
+    }
+}
+
+fn get_window_data(
+    xlib: &x11_dl::xlib::Xlib,
+    window: x11_dl::xlib::Window,
+    display: *mut x11_dl::xlib::Display,
+) -> Result<image::RgbImage>{
+    unsafe {
+        let mut attributes: x11_dl::xlib::XWindowAttributes = unsafe { std::mem::zeroed() };
+        if (xlib.XGetWindowAttributes)(display, window, &mut attributes) == 0 {
+            bail!("Unable to get the window attributes of {:#x}", window)
+        }
+        let width = attributes.width as u32;
+        let height = attributes.height as u32;
+
+        let image = (xlib.XGetImage)(
+            display,
+            window,
+            0,
+            0,
+            width as _,
+            height as _,
+            (xlib.XAllPlanes)(),
+            x11_dl::xlib::ZPixmap as _,
+        );
+        if image.is_null() {
+            bail!("Unable to get the pxiel data from window {:#x}", window)
+        }
+
+        let red_mask = (*image).red_mask;
+        let green_mask = (*image).green_mask;
+        let blue_mask = (*image).blue_mask;
+        let mut imgbuf: image::RgbImage = image::ImageBuffer::new(width, height);
+        for y in 0..height {
+            for x in 0..width {
+                
+                let pixel = (xlib.XGetPixel)(image, x as i32, y as i32);
+                let r = ((pixel & red_mask) >> 16) as u8;
+                let g = ((pixel & green_mask) >> 8) as u8;
+                let b = (pixel & blue_mask) as u8;
+                // https://docs.rs/image/latest/image/struct.ImageBuffer.html
+                imgbuf.put_pixel(x, y, image::Rgb([r, g, b]));
+            }
+        }
+        Ok(imgbuf)
+    }
+}
+
+fn find_window_recursive(
+    xlib: &x11_dl::xlib::Xlib,
+    searched_window_name: &String,
+    window: x11_dl::xlib::Window,
+    display: *mut x11_dl::xlib::Display,
+) -> Result<Option<x11_dl::xlib::Window>> {
+    unsafe {
+        let mut root_return: x11_dl::xlib::Window = 0;
+        let mut parent_return: x11_dl::xlib::Window = 0;
+        let mut children: *mut x11_dl::xlib::Window = std::ptr::null_mut();
+        let mut nchildren: u32 = 0;
+
+        if !(xlib.XQueryTree)(display, window, &mut root_return, &mut parent_return, &mut children, &mut nchildren) == 0 {
+            bail!("Unable to query the root window tree for window {:x}", window)
+        }
+        if children.is_null() {
+            return Ok(None)
+        }
+
+        let child_array = std::slice::from_raw_parts(children, nchildren as usize);
+        for &child in child_array.iter() {
+            let title = get_window_title(&xlib, display, child)?.unwrap_or(String::from("N/A"));
+            println!("Discovering window ID: {:#x} with name [{:?}]", child, title);
+            if title == *searched_window_name {
+                return Ok(Some(child));
+            }
+
+            let res = find_window_recursive(xlib, searched_window_name, child, display)?;
+            if res.is_some() {
+                return Ok(res)
+            }
+        }
+
+        // Free the memory allocated for child windows
+        if !children.is_null() {
+            (xlib.XFree)(children as *mut _);
+        }
+
+        return Ok(None)
+    }
+}
+
+fn get_window_title(
+    xlib: &x11_dl::xlib::Xlib,
+    display: *mut x11_dl::xlib::Display,
+    window: x11_dl::xlib::Window,
+) -> Result<Option<String>> {
+    unsafe {
+        let mut title: Option<String> = None;
+        let mut actual_type_return: x11_dl::xlib::Atom = 0;
+        let mut actual_format_return: std::ffi::c_int = 0;
+        let mut nitems_return: std::ffi::c_ulong = 0;
+        let mut bytes_after_return: std::ffi::c_ulong = 0;
+        let mut data: *mut u8 = std::ptr::null_mut();
+
+        let net_wm_name = std::ffi::CString::new("_NET_WM_NAME".to_string()).unwrap();
+        let utf8_name = std::ffi::CString::new("UTF8_STRING".to_string()).unwrap();
+        let net_wm_atom = (xlib.XInternAtom)(display, net_wm_name.as_ptr(), x11_dl::xlib::False);
+        let utf8_atom = (xlib.XInternAtom)(display, utf8_name.as_ptr(), x11_dl::xlib::False);
+        let ret = (xlib.XGetWindowProperty)(display,
+                                            window,
+                                            net_wm_atom,
+                                            0,
+                                            8096,
+                                            x11_dl::xlib::False,
+                                            utf8_atom,
+                                            &mut actual_type_return,
+                                            &mut actual_format_return,
+                                            &mut nitems_return,
+                                            &mut bytes_after_return,
+                                            &mut data as *mut _,
+        );
+        if ret != x11_dl::xlib::Success as i32 {
+            bail!("Nope");
+        }
+        if !data.is_null() {
+            title = Some(std::ffi::CStr::from_ptr(data as *const i8).to_string_lossy().into_owned());
+            (xlib.XFree)(data as *mut _);
+        }
+
+        Ok(title)
+    }
+}
+
+fn main() {
+    take_screenshot(String::from("~ : zsh — Konsole"))
+        .expect("FATAL ERROR");
+}
